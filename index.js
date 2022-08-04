09e04138509887a07166de5acfb31074c04c64d8
PLLara <freelance.pedrolara@gmail.com>
2022-08-04 10:54:35 -0300

    .

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const request = require("request");
 const server = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
+server.use((0, cors_1.default)());
 server.all("*", (req, res) => {
     try {
         var url = req.query.q || "https://www.slack.com";
--- index.ts
+++ index.ts
@@ -5,8 +5,6 @@ const request = require("request");
 const server = Express();
 const port = process.env.PORT || 3000;
 server.use(cors());
-
-server.use(cors());
 server.all("*", (req: Request, res: Response) => {
   try {
     var url = req.query.q || "https://www.slack.com";
