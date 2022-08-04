f49c4ef3d740228d0bad97fbdf8de854c7b27b51
PLLara <freelance.pedrolara@gmail.com>
2022-08-04 10:48:36 -0300

    .

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import Express, { Request, Response } from "express";
+import cors from 'cors'
 
 const request = require("request");
-
 const server = Express();
 const port = process.env.PORT || 3000;
+server.use(cors());
 
 server.all("*", (req: Request, res: Response) => {
   try {
