44574ccefb4eb66c0bcef5b3e1e4e5f318a62bb7
PLLara <freelance.pedrolara@gmail.com>
2022-08-04 10:52:58 -0300

    .

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import Express, { Request, Response } from "express";
+import cors from 'cors'
 
 const request = require("request");
-
 const server = Express();
 const port = process.env.PORT || 3000;
 
+server.use(cors());
 server.all("*", (req: Request, res: Response) => {
   try {
     var url = req.query.q || "https://www.slack.com";
