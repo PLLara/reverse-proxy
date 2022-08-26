18a8111f699092c9a4503a36e619e33093528dc4
PLLara <freelance.pedrolara@gmail.com>
2022-08-26 15:26:42 -0300

    .

--- index.ts
+++ index.ts
@@ -5,12 +5,15 @@ const request = require("request");
 const server = Express();
 const port = process.env.PORT || 3000;
 
-server.all("*", (req: Request, res: Response) => {
+server.all("*", async (req: Request, res: Response) => {
   try {
     var url = req.query.q || "https://www.slack.com";
-    req.pipe(request.get(url)).pipe(res);
+    const {range} = req.headers
+
+    request.get({ url, headers: {range} }).pipe(res)
+    
   } catch (e) {
-    res.send('Error: ' + e);
+    res.send("Error: " + e);
   }
 });
 
